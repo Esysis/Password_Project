ef8912e5d2b76e853ee1c4cff974eb49da13f813
Yuga <Trev@brennberry.com>
2023-04-20 16:54:22 -0400

    Commented the script that was added in

--- script.js
+++ script.js
@@ -13,24 +13,27 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+// This is where the code I added starts!
+// This is where the function to everything starts by declaring the different character arrays
 function generatePassword() {
   const lower = "abcdefghijklmnopqrstuvwxyz";
   const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numbers = "0123456789";
   const special = "!@#$%^&*()-_=+[]{}|;:',.<>/?`~";
-
+// asks for the password length
   let passwordLength = prompt("Enter the desired password length (between 8 and 128 characters):");
-
+// makes sure the password is between 8 and 128 characters
   while (passwordLength < 8 || passwordLength > 128) {
     alert("Password length must be between 8 and 128 characters.");
     passwordLength = prompt("Enter the desired password length (between 8 and 128 characters):");
   }
-
+// asks what kinds of characters to use when generating the password
   let includeLower = confirm("Include lowercase characters?");
   let includeUpper = confirm("Include uppercase characters?");
   let includeNumbers = confirm("Include numeric characters?");
   let includeSpecial = confirm("Include special characters?");
-
+// checks if atleast one of the character types is selected
   while (!includeLower && !includeUpper && !includeNumbers && !includeSpecial) {
     alert("At least one character type must be selected.");
     includeLower = confirm("Include lowercase characters?");
@@ -38,19 +41,18 @@ function generatePassword() {
     includeNumbers = confirm("Include numeric characters?");
     includeSpecial = confirm("Include special characters?");
   }
-
+// generates the available characters based on what the user chose
   let availableChars = "";
   if (includeLower) availableChars += lower;
   if (includeUpper) availableChars += upper;
   if (includeNumbers) availableChars += numbers;
   if (includeSpecial) availableChars += special;
-
+// officially generates the password
   let generatedPassword = "";
   for (let i = 0; i < passwordLength; i++) {
     const randomIndex = Math.floor(Math.random() * availableChars.length);
     generatedPassword += availableChars[randomIndex];
   }
-
+// returns the finalized generated password
   return generatedPassword;
 }
-// comments :)
